fa06c1d78475a0d124e0f6eac3f72c30e741240f
RypeCreative <40130800+RypeCreative@users.noreply.github.com>
2018-06-21 13:57:17 -0400

    update login/register forms

--- template_parts/members/login_form.php
+++ template_parts/members/login_form.php
@@ -6,9 +6,9 @@
         <div class="col-lg-4 col-lg-offset-4">
 
             <p class="center">
-				<?php esc_html_e( "Don't have an account yet?", 'rypecore' ); ?>
-				<a href="<?php echo esc_url($members_register_page); ?>"><?php esc_html_e( 'Register here!', 'rypecore' ); ?></a>
-			</p>
+                <?php esc_html_e( "Don't have an account yet?", 'rypecore' ); ?>
+                <a href="<?php echo esc_url($members_register_page); ?>"><?php esc_html_e( 'Register here!', 'rypecore' ); ?></a>
+            </p>
 
             <!-- start login form -->
             <div class="login-form">
@@ -35,10 +35,6 @@
         </div><!-- end col -->
     </div><!-- end row -->
 
-<?php } else { ?>
-    <?php $current_user = wp_get_current_user(); ?>
-    <div class="alert-box success">
-    <p><?php esc_html_e( 'You are logged in as', 'rypecore' ); ?> <b><a href="<?php echo get_edit_user_link(); ?>"><?php echo esc_attr($current_user->user_login); ?></a></b></p>
-    <a href="<?php echo wp_logout_url(get_permalink()); ?>"><?php esc_html_e( 'Logout?', 'rypecore' ); ?></a>
-    </div>
-<?php } ?>
+<?php } else { 
+    get_template_part('template_parts/members/alert_logged_in');
+} ?>
--- template_parts/members/register_form.php
+++ template_parts/members/register_form.php
@@ -109,10 +109,6 @@
         </div><!-- end col -->
     </div><!-- end row -->
 
-<?php } else { ?>
-    <?php $current_user = wp_get_current_user(); ?>
-    <div class="alert-box success">
-        <p><?php esc_html_e( 'You are logged in as', 'rypecore' ); ?> <b><a href="<?php echo get_edit_user_link(); ?>"><?php echo esc_attr($current_user->user_login); ?></a></b></p>
-        <a href="<?php echo wp_logout_url(get_permalink()); ?>"><?php esc_html_e( 'Logout?', 'rypecore' ); ?></a>
-    </div>
-<?php } ?>
+<?php } else { 
+    get_template_part('template_parts/members/alert_logged_in');
+} ?>
