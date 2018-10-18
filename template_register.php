484f2a118fd653649b0979aa835bce86bbe05b1b
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2018-10-18 10:26:48 -0400

    updates

--- template_register.php
+++ template_register.php
@@ -5,18 +5,18 @@
 ?>
 
 <?php 
-    $page_id = rypecore_get_page_id();
+    $page_id = ns_core_get_page_id();
     $values = get_post_custom( $page_id );
-    $page_layout = isset( $values['rypecore_page_layout'] ) ? esc_attr( $values['rypecore_page_layout'][0] ) : 'full';
-    $col_class = rypecore_get_page_col_classes($page_layout);
-    $page_layout_widget_area = isset( $values['rypecore_page_layout_widget_area'] ) ? esc_attr( $values['rypecore_page_layout_widget_area'][0] ) : 'blog_sidebar';
-    $page_layout_container = isset( $values['rypecore_page_layout_container'] ) ? esc_attr( $values['rypecore_page_layout_container'][0] ) : 'true';
-    $cta_display = isset( $values['rypecore_cta_display'] ) ? esc_attr( $values['rypecore_cta_display'][0] ) : '';
+    $page_layout = isset( $values['ns_basics_page_layout'] ) ? esc_attr( $values['ns_basics_page_layout'][0] ) : 'full';
+    $col_class = ns_core_get_page_col_classes($page_layout);
+    $page_layout_widget_area = isset( $values['ns_basics_page_layout_widget_area'] ) ? esc_attr( $values['ns_basics_page_layout_widget_area'][0] ) : 'blog_sidebar';
+    $page_layout_container = isset( $values['ns_basics_page_layout_container'] ) ? esc_attr( $values['ns_basics_page_layout_container'][0] ) : 'true';
+    $cta_display = isset( $values['ns_basics_cta_display'] ) ? esc_attr( $values['ns_basics_cta_display'][0] ) : '';
 ?>
 
 <?php get_header() ?>
 
-<?php echo rypecore_generate_page_banner($values); ?>
+<?php echo ns_core_generate_page_banner($values); ?>
 
 <section <?php if($page_layout_container == 'true') { echo 'class="module"'; } ?>>
     <div class="<?php if($page_layout_container != 'true') { echo 'container-fluid'; } else { echo 'container'; } ?>">
@@ -24,16 +24,16 @@
         <div class="row">
             <?php if($page_layout == 'full') { ?>
                 <div class="col-lg-12 <?php if($page_layout_container != 'true') { echo 'col-full'; } ?>">
-					<?php get_template_part('template_parts/loop_page'); ?>
-					<?php get_template_part('template_parts/members/register_form'); ?>
-				</div>
+                    <?php get_template_part('template_parts/loop_page'); ?>
+                    <?php if(function_exists('ns_basics_template_loader')) { ns_basics_template_loader('register_form.php'); } ?>
+                </div>
             <?php } ?>
 
             <?php if($page_layout == 'right sidebar' || $page_layout == 'left sidebar') { ?>
                 <div class="<?php echo $col_class['content']; ?>">
-					<?php get_template_part('template_parts/loop_page'); ?>
-					<?php get_template_part('template_parts/members/register_form'); ?>
-				</div>
+                    <?php get_template_part('template_parts/loop_page'); ?>
+                    <?php if(function_exists('ns_basics_template_loader')) { ns_basics_template_loader('register_form.php'); } ?>
+                </div>
                 <div class="<?php echo $col_class['sidebar']; ?>"><?php if(is_active_sidebar($page_layout_widget_area)) { dynamic_sidebar( $page_layout_widget_area ); } ?></div>
             <?php } ?>
         </div><!-- end row -->
