232e297d4d2b3c2a959acce5a2b73ba8af6bb208
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2018-12-20 13:53:15 -0500

    updates

--- functions.php
+++ functions.php
@@ -66,7 +66,7 @@ function ns_core_admin_scripts() {
         //custom scripts
 		wp_enqueue_script('ns-core-admin-js', get_template_directory_uri() . '/admin/admin.js', array('jquery','media-upload','thickbox', 'wp-color-picker'), '', true);
 		wp_enqueue_style('ns-core-admin-css',  get_template_directory_uri() . '/admin/admin.css', array(), '3.0', 'all');
-    	wp_enqueue_style('font-awesome',  get_template_directory_uri() . '/css/font-awesome/css/all.min.css', array(), '', 'all');
+    	wp_enqueue_style('ns-font-awesome',  get_template_directory_uri() . '/css/font-awesome/css/all.min.css', array(), '', 'all');
         wp_enqueue_script( 'chosen', get_template_directory_uri() . '/assets/chosen-1.6.2/chosen.jquery.min.js', array( 'jquery' ), '', true );
         wp_enqueue_style('chosen',  get_template_directory_uri() . '/assets/chosen-1.6.2/chosen.min.css', array(), '', 'all');
 
@@ -125,7 +125,7 @@ function ns_core_load_stylesheets() {
 		wp_enqueue_style('bootstrap',  get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.0', 'all');
         wp_enqueue_style('slick-slider',  get_template_directory_uri() . '/assets/slick-1.6.0/slick.css', array(), '', 'all');
         wp_enqueue_style('chosen',  get_template_directory_uri() . '/assets/chosen-1.6.2/chosen.min.css', array(), '', 'all');
-		wp_enqueue_style('font-awesome',  get_template_directory_uri() . '/css/font-awesome/css/all.min.css', array(), '', 'all');
+		wp_enqueue_style('ns-font-awesome',  get_template_directory_uri() . '/css/font-awesome/css/all.min.css', array(), '', 'all');
 		wp_enqueue_style('linear-icons',  get_template_directory_uri() . '/assets/linear-icons/style.css', array(), '', 'all');
         wp_enqueue_style('dripicons',  get_template_directory_uri() . '/assets/dripicons/webfont.css', array(), '', 'all');
         wp_enqueue_style('fancybox',  get_template_directory_uri() . '/assets/fancybox/dist/jquery.fancybox.min.css', array(), '', 'all');
