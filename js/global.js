1d0b94d9cd0b33efa85ebb6b1ed6a68ed7e06082
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-02-14 00:02:33 -0500

    Update global.js

--- js/global.js
+++ js/global.js
@@ -16,20 +16,24 @@ jQuery(document).ready(function($) {
         event.preventDefault();
         $('body').find('.mobile-overlay').toggle();
         var menu = $(this).closest('header').find('.main-menu');
-        menu.toggleClass('mobile open');
-        menu.animate({width:'toggle'}, 350);
+
+        if(menu.hasClass('open')) {
+            menu.fadeOut('fast', function() {
+                menu.removeClass('mobile open');
+            });
+        } else {
+            menu.addClass('mobile open');
+            menu.animate({width:'toggle'}, 350);
+        }
     });
 
     $('.mobile-overlay, .main-menu-close').on('click', function(event) {
         event.preventDefault();
         $('.mobile-overlay').hide();
         var menu = $('header').find('.main-menu');
-        menu.animate({
-            width:'toggle'
-        }, 350, function() {
-            menu.removeClass('mobile open');
-        });
+        menu.fadeOut('fast', function() { menu.removeClass('mobile open'); });
     });
+
 
 	/***************************************************************************/
 	//MAIN MENU SUB MENU TOGGLE
