3c62607a3fda96856c6d832a179d67600a889be8
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-02-13 22:47:52 -0500

    Update header.php

--- header.php
+++ header.php
@@ -63,10 +63,8 @@ if($header_vars['header_container'] != 'true') { $header_class = $header_class.'
 
         <?php 
         if($header_style == 'default') {
-            //include('template_parts/header-menu-bar.php');
             get_template_part('template_parts/header-menu-bar');
         } else {
-            //include('template_parts/header-classic.php');
             get_template_part('template_parts/header-classic');
         }
         ?>
